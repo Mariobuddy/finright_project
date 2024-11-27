48c634a1bb4d4eeea2d25add47b0297fef9c4bfb
Rohit Bhatt <rohitbhatt@q2pay.in>
2024-11-27 17:03:50 +0530

    #npm install

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DragonGame from './Components/DragonGame';
+import "./index.css"
+
+const App = () => {
+  return (
+    <div style={{height:"100vh",display:"flex",justifyContent:"center",alignItems:"center",backgroundColor:"#000000"}}>
+      <DragonGame/>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/DragonGame/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./index.css"
+const DragonGame = () => {
+  return (
+    <div className='gameMainDiv'>
+    </div>
+  )
+}
+
+export default DragonGame;
